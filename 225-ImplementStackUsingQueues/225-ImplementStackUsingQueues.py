9c80566fde938a958cfa5432a25381f34af09ef1
Juro Zaw <kaung516177@gmail.com>
2025-05-27 18:45:49 -0400

    Create 225-ImplementStackUsingQueues

---
+++ 225-ImplementStackUsingQueues/225-ImplementStackUsingQueues.py
@@ -0,0 +1,29 @@
+# Last updated: 5/27/2025, 6:45:50 PM
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        # Move elements to the back until the last one is at the front
+        for _ in range(len(self.q) - 1):
+            self.push(self.q.popleft())
+        # Pop that one
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
