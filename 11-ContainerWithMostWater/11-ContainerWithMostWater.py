31213824e4d07fb8ff8e6a341cb2098ff4cbec39
Juro Zaw <kaung516177@gmail.com>
2025-07-08 21:10:30 -0400

    File updated at 7/8/2025, 9:10:29 PM

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.py
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.py
@@ -1,4 +1,4 @@
-# Last updated: 7/8/2025, 9:09:21 PM
+# Last updated: 7/8/2025, 9:10:29 PM
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         left = 0
@@ -12,18 +12,8 @@ class Solution:
             area = w * h
             res = max(res, area)
 
-            # Move the pointers if..
-            # diff1 = abs(height[left] - height[right - 1])
-            # diff2 = abs(height[right] - height[left + 1])
-
-            # if diff1 >= diff2:
             if height[left] < height[right]:
                 left += 1
-                # if height[left + 1] == height[right - 1]:
-                #     if height[left] > height[right]:
-                #         right -= 1
-                #     else:
-                #         left += 1
             else:
                 right -= 1
         return res
